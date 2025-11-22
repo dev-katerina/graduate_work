f203270414a02f708dfab53122d531bec40570dc
dev-katerina <sunshine9474379@gmail.com>
2025-11-22 12:29:45 +0300

    4 пункт - backoff

--- voice_router/src/connections/elastic.py
+++ voice_router/src/connections/elastic.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from elasticsearch import AsyncElasticsearch
+from elasticsearch.exceptions import ConnectionError, TransportError, NotFoundError
 
+RETRIABLE_EXCEPTIONS = (ConnectionError, TransportError, NotFoundError)
 es: Optional[AsyncElasticsearch] = None
 
 
